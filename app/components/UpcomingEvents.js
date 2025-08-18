fc1759ff3ad12421679919fb31e004751f37e4c1
jkoefler <joris.koefler@mis.mpg.de>
2025-08-18 17:18:03 +0200

    fixed white box changing

--- app/components/UpcomingEvents.js
+++ app/components/UpcomingEvents.js
@@ -29,26 +29,30 @@ export default function UpcomingEvents() {
 function Event({ days, month, year, location, link, title }) {
   return (
     <div className="flex flex-col shadow-lg max-w-2xl">
-
-    <div className="flex flex-col sm:flex-row rounded-lg bg-black sm:items-center sm:max-h-52 gap-6 p-5 sm:pl-5 sm:pr-9 py-5">
-      <div className="rounded py-2 border-zinc-100 border-2 sm:border-none sm:bg-zinc-100 flex flex-col justify-center sm:gap-2 text-center sm:aspect-square sm:w-[150px]">
-        <span className="text-zinc-100 sm:text-black font-semibold text-2xl">{days}</span>
-        <span className="text-zinc-100 sm:text-black font-semibold text-2xl">{month} {year}</span>
-      </div>
-
-      <div className="text-white flex flex-col gap-4">
-        <div className="flex flex-col gap-1">
-          <Link className="font-bold text-xl" href={link}>{title}</Link>
-          <span className="font-thin">{location}</span>
+      {/* sm:items-center vertically centers the two columns on larger screens */}
+      <div className="flex flex-col sm:flex-row rounded-lg bg-black sm:items-center gap-6 p-5 sm:pl-5 sm:pr-9 py-5">
+        {/* remove aspect-square / h-full; use a fixed width and self-center to vertically center */}
+        <div className="rounded py-4 px-6 border-zinc-100 border-2 sm:border-none sm:bg-zinc-100 flex flex-col justify-center sm:gap-2 text-center sm:w-[150px] sm:self-center">
+          <span className="text-zinc-100 sm:text-black font-semibold text-2xl">{days}</span>
+          <span className="text-zinc-100 sm:text-black font-semibold text-2xl">{month} {year}</span>
         </div>
-        <button className="flex flex-row items-center gap-2">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
-            <path fill-rule="evenodd" d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
-          </svg>
-          <Link href={link} class="text-base">Visit website</Link>
-        </button>
+
+        <div className="text-white flex-1 flex flex-col justify-between gap-4">
+          <div className="flex flex-col gap-1">
+            <Link href={link} className="font-bold text-xl">{title}</Link>
+            <span className="font-thin">{location}</span>
+          </div>
+
+          <div>
+            <button className="flex flex-row items-center gap-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5" aria-hidden>
+                <path fillRule="evenodd" d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+              </svg>
+              <Link href={link} className="text-base">Visit website</Link>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
   );
 }
