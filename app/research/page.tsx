18d1ac6757a73e68bc39e272549c9a2e38a5ba18
jkoefler <joris.koefler@mis.mpg.de>
2026-03-04 17:02:21 +0100

    h-screen bug

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -5,8 +5,9 @@ export default function Research() {
     return (
         <div className="flex flex-col gap-14 pb-20 lg:mx-16">
             <h1 className="text-balance font-extrabold text-4xl ml-[23px]">
-            Research
+                Research
             </h1>
+            
             <div className="relative">
             <Publications/>
             </div>
--- app/teaching/page.tsx
+++ app/teaching/page.tsx
@@ -3,11 +3,11 @@ import Courses from "@/app/components/courses.js";
 
 export default function Teaching() {
     return (
-        <div className="flex flex-col gap-14 pb-20 lg:mx-16">
+        <div className="flex flex-col gap-14 pb-20 lg:mx-16 relative">
             <h1 className="text-balance font-extrabold text-4xl ml-[23px]">
             Teaching
             </h1>
-            <div className="flex items-center justify-center h-screen">
+            <div className="flex items-center justify-center">
             <Courses/>
         </div>
     </div>
